fe99a2888e59ac330f6b41c0100856599da9fede
Andrew Chappell <A.Chappell@warwick.ac.uk>
2026-08-13 09:34:15 +0100

    Plumb in I/O for new geometry information

--- larpandoracontent/LArPersistency/EventReadingAlgorithm.cc
+++ larpandoracontent/LArPersistency/EventReadingAlgorithm.cc
@@ -15,6 +15,7 @@
 #include "larpandoracontent/LArObjects/LArMCParticle.h"
 
 #include "larpandoracontent/LArPersistency/EventReadingAlgorithm.h"
+#include "larpandoracontent/LArPersistency/LArTPCFactory.h"
 
 #include <algorithm>
 
@@ -51,6 +52,7 @@ StatusCode EventReadingAlgorithm::Initialize()
         {
             BinaryFileReader fileReader(this->GetPandora(), m_geometryFileName);
             this->ConfigureReader(fileReader);
+            fileReader.SetFactory(new LArTPCFactory());
             PANDORA_RETURN_RESULT_IF(pandora::STATUS_CODE_SUCCESS, !=, fileReader.ReadGlobalHeader());
             PANDORA_RETURN_RESULT_IF(pandora::STATUS_CODE_SUCCESS, !=, fileReader.ReadGeometry());
         }
@@ -58,6 +60,7 @@ StatusCode EventReadingAlgorithm::Initialize()
         {
             XmlFileReader fileReader(this->GetPandora(), m_geometryFileName);
             this->ConfigureReader(fileReader);
+            fileReader.SetFactory(new LArTPCFactory());
             PANDORA_RETURN_RESULT_IF(pandora::STATUS_CODE_SUCCESS, !=, fileReader.ReadGlobalHeader());
             PANDORA_RETURN_RESULT_IF(pandora::STATUS_CODE_SUCCESS, !=, fileReader.ReadGeometry());
         }
